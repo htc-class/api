4b7b5aa71ad3f2946df5907f087744ba70fa9737
Jared Henderson <jared@netrivet.com>
2020-08-06 14:44:28 -0400

    update endpoints

--- create-json.ts
+++ create-json.ts
@@ -10,12 +10,33 @@ export default async function handler(): Promise<void> {
   friends.forEach((f) => {
     const fr: any = f.toJSON();
     fr.documents = f.documents.map((d) => d.id);
-    friendsMap[f.id] = fr;
+    friendsMap[f.id] = {
+      id: fr.id,
+      name: fr.name,
+      alphabeticalName: fr.alphabeticalName,
+      gender: fr.gender === `male` ? `male` : `female`,
+      born: fr.born || null,
+      died: fr.died || null,
+      description: fr.description,
+      residence: fr.primaryResidence || null,
+      documents: fr.documents,
+      url: `https://www.friendslibrary.com/friend/${fr.slug}`,
+    };
     f.documents.forEach((d) => {
+      const docJson = d.toJSON();
       docsMap[d.id] = {
+        id: docJson.id,
+        title: docJson.title,
+        description: docJson.description,
+        shortDescription: docJson.partialDescription,
+        tags: docJson.tags,
         friendId: f.id,
-        ...d.toJSON(),
-        editions: d.editions.map((e) => ({ type: e.type, isbn: e.isbn })),
+        editions: d.editions.map((e) => ({
+          type: e.type,
+          isbn: e.isbn,
+          imageUrl: `http://flp-assets.nyc3.digitaloceanspaces.com/en/${f.slug}/${docJson.slug}/${e.type}/${e.filenameBase}--audio.png`,
+        })),
+        url: `https://www.friendslibrary.com/${f.slug}/${docJson.slug}`,
       };
     });
   });
